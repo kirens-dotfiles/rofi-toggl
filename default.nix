4568f6ba50f7373bf30f4bd1b741f456fb48dc48
Erik Nygren <dev@erik.work>
2019-07-01 14:45:14 +0200

    Updated package expression with node2nix
    This should not requre a node_modules directory

--- default.nix
+++ default.nix
@@ -1,21 +1,29 @@
-{ stdenv, nodejs, bash, rofi }:
+{pkgs ? import <nixpkgs> {
+    inherit system;
+  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs-10_x"
+, bash ? pkgs.bash, rofi ? pkgs.rofi }:
 
-stdenv.mkDerivation {
-  name = "rofi-toggl";
-  src = ./.;
-
-  installPhase = ''
+let
+  nodeEnv = import ./node-env.nix {
+    inherit (pkgs) stdenv python2 utillinux runCommand writeTextFile;
+    inherit nodejs;
+    libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
+  };
+  initial = import ./node-packages.nix {
+    inherit (pkgs) fetchurl fetchgit;
+    inherit nodeEnv;
+  };
+in initial // {
+  package = initial.package.overrideAttrs (_: { postInstall = ''
     mkdir -p $out/bin
     cat > $out/bin/rofi-toggl <<EOL
-      #! ${bash}/bin/bash
-      export NODE_PATH=$out/lib/rofi-toggl/node_modules
-      ${nodejs}/bin/node $out/lib/rofi-toggl/index.js
+    #! ${bash}/bin/bash
+    export NODE_PATH=$out/lib/node_modules/rofi-toggl/node_modules
+    ${nodejs}/bin/node $out/lib/node_modules/rofi-toggl/index.js
     EOL
     chmod +x $out/bin/rofi-toggl
 
-    mkdir -p $out/lib/rofi-toggl
-    cp -r index.js node_modules $out/lib/rofi-toggl/
-    substituteInPlace $out/lib/rofi-toggl/index.js \
+    substituteInPlace $out/lib/node_modules/rofi-toggl/index.js \
       --replace "rofiExecutable = 'rofi'" "rofiExecutable = '${rofi}/bin/rofi'"
-  '';
+  ''; });
 }
